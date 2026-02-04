caa5133883d7b1375291171f43c5845d74ab0c83
anirudhchakraborty <preetiujjal@gmail.com>
2026-02-04 21:21:09 +0530

    Fixed Backend Bug

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
